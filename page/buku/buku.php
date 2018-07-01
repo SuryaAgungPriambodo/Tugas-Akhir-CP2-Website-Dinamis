8f16e54725013dad92ad2f77d2ebed3a24fa02bb
Surya Agung Priambodo <37823431+SuryaAgungPriambodo@users.noreply.github.com>
2018-07-01 11:10:48 +0700

    Tambah halaman tambah data (tambah.php)

--- page/buku/buku.php
+++ page/buku/buku.php
@@ -1,4 +1,4 @@
-<a href =?page=buku&aksi=tambah class = "btn btn-primary" style = "margin-bottom:5px;">Tambah Buku</a>
+<a href ="?page=buku&aksi=tambah" class = "btn btn-primary" style = "margin-bottom:5px;">Tambah Data</a>
 
 <div class="row">
     <div class="col-md-12">
@@ -6,42 +6,48 @@
         <div class="panel panel-default">
             <div class="panel-heading">
                 Data Buku
-           </div>
-           <div class="panel-body">
-            <div class="table-responsive">
-                <table class="table table-striped table-bordered table-hover" id="dataTables-example">
-                    <thead>
-                        <tr>
-                            <th>No</th>
-                            <th>Judul</th>
-                            <th>Pengarang</th>
-                            <th>Penerbit</th>
-                            <th>ISBN</th>
-                            <th>Jumlah Buku</th>
-                            <th>Aksi</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-
-                        $no =1;
-                        $sql = $koneksi->query("select * from tb_buku");
-
-                        while ($data = $sql->fetch_assoc()){
-                            ?>
-
+            </div>
+            <div class="panel-body">
+                <div class="table-responsive">
+                    <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                        <thead>
                             <tr>
-                                <td><?php echo $no++;?></td>
-                                <td><?php echo $data['judul'];?></td>
-                                <td><?php echo $data['pengarang'];?></td>
-                                <td><?php echo $data['penerbit'];?></td>
-                                <td><?php echo $data['isbn'];?></td>
-                                <td><?php echo $data['jumlah_buku'];?></td>
-                                <td>
-                                    <a href = "" class = "btn btn-info">Ubah</a>
-                                    <a href = "" class = "btn btn-danger">Hapus</a>
-                                </td>
+                                <th>No</th>
+                                <th>Judul</th>
+                                <th>Pengarang</th>
+                                <th>Penerbit</th>
+                                <th>ISBN</th>
+                                <th>Jumlah Buku</th>
+                                <th>Aksi</th>
                             </tr>
+                        </thead>
+                        <tbody>
+                            <?php
 
-                        <?php }?> 
-                    </tbody>
+                            $no =1;
+                            $sql = $koneksi->query("select * from tb_buku");
+
+                            while ($data = $sql->fetch_assoc()){
+                                ?>
+
+                                <tr>
+                                    <td><?php echo $no++;?></td>
+                                    <td><?php echo $data['judul'];?></td>
+                                    <td><?php echo $data['pengarang'];?></td>
+                                    <td><?php echo $data['penerbit'];?></td>
+                                    <td><?php echo $data['isbn'];?></td>
+                                    <td><?php echo $data['jumlah_buku'];?></td>
+                                    <td>
+                                        <a href = "" class = "btn btn-info">Ubah</a>
+                                        <a href = "" class = "btn btn-danger">Hapus</a>
+                                    </td>
+                                </tr>
+
+                            <?php }?> 
+                        </tbody>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
---
+++ page/buku/tambah.php
@@ -0,0 +1,70 @@
+<div class="panel panel-default">
+	<div class="panel-heading">
+		Tambah Data
+	</div>
+	<div class="panel-body">
+		<div class="row">
+			<div class="col-md-12">
+				<form role="form">
+					<div class="form-group">
+						<label>Judul</label>
+						<input class="form-control" name = "judul"/>
+					</div>
+
+					<div class="form-group">
+						<label>Pengarang</label>
+						<input class="form-control" name = "pengarang"/>
+					</div>
+
+					<div class="form-group">
+						<label>Penerbit</label>
+						<input class="form-control" name = "penerbit"/>
+					</div>
+
+					<div class="form-group">
+						<label>Tahun Terbit</label>
+						<select class="form-control" name = "tahun">
+							<?php
+
+							$tahun =date("Y");
+
+							for ($i=$tahun-29; $i <= $tahun; $i++) {
+								echo '<option value="'.$i.'">'.$i.'</option>';
+							}
+
+							?>
+						</select>
+					</div>
+
+					<div class="form-group">
+						<label>ISBN</label>
+						<input class="form-control" name = "isbn"/>
+					</div>
+
+					<div class="form-group">
+						<label>Jumlah Buku</label>
+						<input class="form-control" type = "number" name = "jumlah"/>
+					</div>
+
+					<div class="form-group">
+						<label>Lokasi</label>
+						<select class="form-control" name = "lokasi">
+							<option value = "rak1">Rak 1</option>
+							<option value = "rak2">Rak 2</option>
+							<option value = "rak3">Rak 3</option>
+						</select>
+					</div>
+
+					<div class="form-group">
+						<label>Tanggal Input</label>
+						<input class="form-control" name = "tanggal"/>
+					</div>
+
+					<div>
+						<input type = "submit" name="simpan" value = "Simpan" class="btn - btn-primary">
+					</div>
+
+				</div>
+			</div>
+		</div>
+	</div>
