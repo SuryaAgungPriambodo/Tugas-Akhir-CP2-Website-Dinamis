aea63d2b0b2b7af84076771d8a99231d554e92fc
Surya Agung Priambodo <37823431+SuryaAgungPriambodo@users.noreply.github.com>
2018-07-02 09:58:49 +0700

    Tambah halaman anggota (anggota.php)

--- page/anggota/anggota.php
+++ page/anggota/anggota.php
@@ -1,1 +1,55 @@
-anggota
+<a href ="?page=buku&aksi=tambah" class = "btn btn-primary" style = "margin-bottom:5px;">Tambah Data</a>
+
+<div class="row">
+    <div class="col-md-12">
+        <!-- Advanced Tables -->
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                Data Buku
+            </div>
+            <div class="panel-body">
+                <div class="table-responsive">
+                    <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Nama</th>
+                                <th>NIM</th>
+                                <th>Tempat Lahir</th>
+                                <th>Tanggal Lahir</th>
+                                <th>Jenis Kelamin</th>
+                                <th>Prodi</th>
+                                <th>Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+
+                            $no =1;
+                            $sql = $koneksi->query("select * from tb_anggota");
+
+                            while ($data = $sql->fetch_assoc()){
+                                ?>
+
+                                <tr>
+                                    <td><?php echo $no++;?></td>
+                                    <td><?php echo $data['nama'];?></td>
+                                    <td><?php echo $data['nim'];?></td>
+                                    <td><?php echo $data['tempat_lahir'];?></td>
+                                    <td><?php echo $data['tgl_lahir'];?></td>
+                                    <td><?php echo $data['jk'];?></td>
+                                    <td><?php echo $data['prodi'];?></td>
+                                    <td>
+                                        <a href = "?page=buku&aksi=ubah&id=<?php echo $data['id'];?>" class = "btn btn-info">Ubah</a>
+                                        <a onclick = "return confirm('Anda Yakin Ingin Menghapus Data Ini ?')"href = "?page=buku&aksi=hapus&id=<?php echo $data['id'];?>" class = "btn btn-danger">Hapus</a>
+                                    </td>
+                                </tr>
+
+                            <?php }?> 
+                        </tbody>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
